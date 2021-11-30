28fd74bc274247c0929f012e83247546e16b88f2
Dzénetan Massart <46716929+DzenetanMassart@users.noreply.github.com>
2021-11-30 15:51:21 +0100

    Amélioration du jeu de sélection avec les flèches

--- assets/js/choix.js
+++ assets/js/choix.js
@@ -18,6 +18,25 @@ function openPage(pageName, elmnt, color) {
     // Add the specific color to the button used to open the tab content
     elmnt.style.backgroundColor = color;
 }
+function openPage_arrow(pageName, elmnt, color) {
+    // Hide all elements with class="tabcontent" by default */
+    let i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Remove the background color of all tablinks/buttons
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    }
+
+    // Show the specific tab content
+    document.getElementById(pageName).style.display = "flex";
+
+
+}
 
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
@@ -27,12 +46,18 @@ document.onkeydown = checkKey;
 function checkKey(e) {
 e = e || window.event;
 
-    if (e.keyCode == '38') {
-        openPage('meto', this, 'green')
+    if (e.keyCode == '37') {
+        openPage_arrow('prat', this, 'red');
+        document.querySelectorAll(".tablink")[0].style.backgroundColor = "red";
     }
-    else if (e.keyCode == '37') {
-        openPage('prat', this, 'red');
+    else if (e.keyCode == '38') {
+        openPage_arrow('meto', this, 'green');
+        document.querySelectorAll(".tablink")[1].style.backgroundColor = "green";
+
     }
     else if (e.keyCode == '39') {
-        openPage('theo', this, 'blue')
+        openPage_arrow('theo', this, 'blue');
+        document.querySelectorAll(".tablink")[2].style.backgroundColor = "blue";
+
+
     }}
